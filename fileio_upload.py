ec8b8b6381b70bd9eca26b2d7c14d5dcefd9f7cf
David Chen <davidc3287@gmail.com>
2020-06-17 15:14:20 -0400

    add file.io upload, Join push, and pyperclip functionality

--- fileio_upload.py
+++ fileio_upload.py
@@ -1,8 +1,46 @@
 """
-Upload file to file.io and set clipboard to url
+A command line utility to allow for easy file transfers.
+
+Usage: python3 fileio_upload.py test.txt
+This command will print the file.io link and will also copy the link to clipboard.
+Optional flag -j will push the link the a phone connected through Join by Joaoapps.
+
+Author: David Chen
 """
 import requests
+import json
+import pyperclip
 
-url = 'https://file.io/'
+# this should be of the basic form below:
+# https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceId=???&apikey=???
+JOIN_API_URL = 'https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceId=7a050437f66742919d68fb6b51b9c230&apikey=a174dac143094445a35aaf717c2dd875'
 
-resp = requests.post(url, )
+
+def get_link_from_resp(resp):
+    """ Return file.io link from API response """
+    return json.loads(resp.content)['link']
+
+
+def upload_to_fileio(file_name):
+    """ Upload file to file.io, return response """
+    url = 'https://file.io/'
+
+    files = {
+        'file': ('test.txt', open('test.txt', 'rb')),
+    }
+
+    resp = requests.post(url, files=files)
+    return resp
+
+
+def join_push_url(url):
+    """ Push URL to to device with API key as specified in JOIN_API_URL """
+    url = JOIN_API_URL + '&url=' + url
+    requests.get(url)
+
+
+if __name__ == '__main__':
+    resp = upload_to_fileio('test.txt')
+    link = get_link_from_resp(resp)
+    pyperclip.copy(link)
+
